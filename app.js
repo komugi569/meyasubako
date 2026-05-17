f4c30c64f635c55532097ba5f35fa78b7f426cb8
komugi <junichirousagawa@gmail.com>
2026-05-17 12:16:57 +0900

    これでどうだ！！

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from
 
 // 💡 【重要】あなたのFirebaseコンソールからコピーしたConfigをここに貼り付けてください！
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
